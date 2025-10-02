bb2bb54ee86b280f7be3b4c4b79e5f1f00049c5e
ScalierBullet63 <199808687+ScalierBullet63@users.noreply.github.com>
2025-10-02 18:38:29 +0200

    Include Clap dependencies and argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Target file
+    #[arg(short, long)]
+    target_file: Option<String>,
+
+    /// Message to hide
+    #[arg(short, long)]
+    hidden_message: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.target_file {
+        Some(name) => println!("Target file: {}", name),
+        None => println!("No target file specified!"),
+    }
 }
