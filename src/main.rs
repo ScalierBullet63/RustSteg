23795c6e2d3d7b3101bc486bdb531ab4dd674dcf
ScalierBullet63 <199808687+ScalierBullet63@users.noreply.github.com>
2025-10-09 20:17:57 +0200

    Improve image reading

--- src/main.rs
+++ src/main.rs
@@ -28,12 +28,12 @@ fn main() {
         }
     };
 
-    println!("{:?}", target_file);
+    println!("Selected {}", target_file);
 
-    let img: DynamicImage = match image_reader(target_file) {
-        Ok(image) => {
+    let (img, width, height) = match image_reader(target_file) {
+        Ok((image, width, height)) => {
             println!("Success!");
-            image
+            (image, width, height)
         }
         Err(e) => {
             println!("Error loading image: {}", e);
@@ -41,10 +41,33 @@ fn main() {
         }
     };
 
-    println!("{:?}", img);
+    // println!("{:?}", img);
+    for y in 0usize..height as usize {
+        for x in 0usize..width as usize {
+            print!("{:?}", img[y][x]);
+        }
+        println!();
+        println!();
+        println!();
+    }
 }
 
-fn image_reader(target_file: String) -> Result<DynamicImage, ImageError> {
-    let img = ImageReader::open(target_file)?.decode()?;
-    Ok(img)
+fn image_reader(target_file: String) -> Result<(Vec<Vec<[u8; 4]>>, u32, u32), ImageError> {
+    let img: DynamicImage = ImageReader::open(target_file)?.decode()?;
+
+    let rgba_image: image::ImageBuffer<image::Rgba<u8>, Vec<u8>> = img.to_rgba8();
+    let (width, height) = rgba_image.dimensions();
+
+    let mut rgba_image_matrix: Vec<Vec<[u8; 4]>> = Vec::new();
+
+    for y in 0..height {
+        let mut row: Vec<[u8; 4]> = Vec::new();
+        for x in 0..width {
+            let pixel = rgba_image.get_pixel(x, y);
+            row.push(pixel.0);
+        }
+        rgba_image_matrix.push(row);
+    }
+
+    Ok((rgba_image_matrix, width, height))
 }
